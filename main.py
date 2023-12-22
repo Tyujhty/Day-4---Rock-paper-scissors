ee03b5ef31fe237e873633880d22620404f8a1c0
Tyujhty <fannybaquey@gmail.com>
2023-12-22 08:48:12 +0000

    feat: changing the list choices by a list of variables. Delete the print condition to return the choice

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ scissors = '''
 #Write your code below this line 👇
 import random
 
-choices = ["rock", "paper", "scissors"]
+choices = [rock, paper, scissors]
 
 print("Welcom to the ROCK, PAPER, SCISSORS PYTHON GAME \n\n")
 
@@ -37,23 +37,11 @@ user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 f
 object_user_choice = choices[user_choice]
 
 print(f"\nThe user has chosen: {object_user_choice} ")
-if user_choice == 0:
-  print(rock)
-elif user_choice == 1:
-  print(paper)
-else:
-  print(scissors)
 
 computer_choice = random.choice(choices)
 computer_choice_index = choices.index(computer_choice)
 
 print (f"The computer has chosen: {computer_choice}")
-if computer_choice == "rock":
-  print(rock)
-elif computer_choice == "paper":
-  print(paper)
-else:
-  print(scissors)
 
 if (computer_choice == "rock" and object_user_choice == "scissors") or (computer_choice == "scissors" and object_user_choice == "paper") or ( computer_choice == "paper" and object_user_choice == "rock"):
   print("The computer win :(")
